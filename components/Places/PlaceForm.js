d7051d9ae6ddc5a794d25475aa1136b623436b00
Dagim Asnake <dagimasnake1@gmail.com>
2023-08-10 01:06:22 -1200

    configuring image picker

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView, TextInput } from "react-native";
+import { useState } from "react";
+
+import { Colors } from "../../constants/colors";
 
 function PlaceForm() {
+    const [enteredTitle, setEnteredTitel] = useState("")
+
+    function changeTitelHandler(enteredText) {
+        setEnteredTitel(enteredText)
+    }
+
     return (
-        <View>
-            <Text>This is the place form</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput 
+                    style={styles.input} 
+                    onChangeText={changeTitelHandler} 
+                    value={enteredTitle} 
+                />
+            </View>
+        </ScrollView>
     )
 }
 
 export default PlaceForm
+
+const styles = StyleSheet.create({
+    form: {
+        flex: 1,
+        padding: 24,
+    },
+    label: {
+        fontWeight: "bold",
+        marginBottom: 4,
+        color: Colors.primary500
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100
+    }
+})
